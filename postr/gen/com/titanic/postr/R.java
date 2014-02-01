a1b6f896735e4040edb4ef9e18e842abaaa1aae5
Ashna Ashok <aashok@andew.cmu.edu>
2014-02-01 02:08:08 -0500

    fixing breakages

--- postr/gen/com/titanic/postr/R.java
+++ postr/gen/com/titanic/postr/R.java
@@ -10,6 +10,14 @@ package com.titanic.postr;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int black=0x7f040000;
+        public static final int blue=0x7f040001;
+        public static final int custom_button=0x7f040004;
+        public static final int custom_text=0x7f040005;
+        public static final int red=0x7f040002;
+        public static final int white=0x7f040003;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -17,26 +25,27 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int BackPic=0x7f08000a;
-        public static final int Cancel=0x7f080009;
-        public static final int EditEventName=0x7f080002;
-        public static final int EndDate=0x7f080007;
-        public static final int EventName=0x7f080001;
-        public static final int Location=0x7f080003;
-        public static final int ScrollView01=0x7f080000;
-        public static final int ShowEndDate=0x7f080008;
-        public static final int ShowLocation=0x7f080004;
-        public static final int ShowStartDate=0x7f080006;
-        public static final int StartDate=0x7f080005;
-        public static final int Submit=0x7f08000b;
-        public static final int action_settings=0x7f08000c;
+        public static final int BackPic=0x7f09000b;
+        public static final int Cancel=0x7f09000a;
+        public static final int EditEventName=0x7f090003;
+        public static final int EndDate=0x7f090008;
+        public static final int EventName=0x7f090002;
+        public static final int Location=0x7f090004;
+        public static final int ScrollView01=0x7f090001;
+        public static final int ShowEndDate=0x7f090009;
+        public static final int ShowLocation=0x7f090005;
+        public static final int ShowStartDate=0x7f090007;
+        public static final int StartDate=0x7f090006;
+        public static final int Submit=0x7f09000c;
+        public static final int action_settings=0x7f09000d;
+        public static final int imgLogo=0x7f090000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -44,24 +53,24 @@ public final class R {
         public static final int form_layout=0x7f030002;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int backpic=0x7f05000e;
-        public static final int cancel=0x7f05000d;
-        public static final int editeventname=0x7f050004;
-        public static final int end_date=0x7f05000a;
-        public static final int eventname=0x7f050003;
-        public static final int hello_world=0x7f050002;
-        public static final int show_date=0x7f05000b;
-        public static final int show_end_date=0x7f050008;
-        public static final int show_start_date=0x7f050007;
-        public static final int show_where=0x7f050006;
-        public static final int start_date=0x7f050009;
-        public static final int submit=0x7f05000c;
-        public static final int where=0x7f050005;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int backpic=0x7f06000e;
+        public static final int cancel=0x7f06000d;
+        public static final int editeventname=0x7f060004;
+        public static final int end_date=0x7f06000a;
+        public static final int eventname=0x7f060003;
+        public static final int hello_world=0x7f060002;
+        public static final int show_date=0x7f06000b;
+        public static final int show_end_date=0x7f060008;
+        public static final int show_start_date=0x7f060007;
+        public static final int show_where=0x7f060006;
+        public static final int start_date=0x7f060009;
+        public static final int submit=0x7f06000c;
+        public static final int where=0x7f060005;
     }
     public static final class style {
         /** 
@@ -85,10 +94,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
