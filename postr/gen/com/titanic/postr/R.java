66431be83e23256e6e73eb87785728b86fe48a98
Ashna Ashok <aashok@andew.cmu.edu>
2014-02-01 02:40:40 -0500

    adding splash screen

--- postr/gen/com/titanic/postr/R.java
+++ postr/gen/com/titanic/postr/R.java
@@ -30,6 +30,7 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int logo=0x7f020001;
     }
     public static final class id {
         public static final int BackPic=0x7f09000b;
@@ -45,7 +46,7 @@ public final class R {
         public static final int StartDate=0x7f090006;
         public static final int Submit=0x7f09000c;
         public static final int action_settings=0x7f09000d;
-        public static final int imgLogo=0x7f090000;
+        public static final int logo=0x7f090000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -58,19 +59,20 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f060001;
         public static final int app_name=0x7f060000;
-        public static final int backpic=0x7f06000e;
-        public static final int cancel=0x7f06000d;
-        public static final int editeventname=0x7f060004;
-        public static final int end_date=0x7f06000a;
-        public static final int eventname=0x7f060003;
-        public static final int hello_world=0x7f060002;
-        public static final int show_date=0x7f06000b;
-        public static final int show_end_date=0x7f060008;
-        public static final int show_start_date=0x7f060007;
-        public static final int show_where=0x7f060006;
-        public static final int start_date=0x7f060009;
-        public static final int submit=0x7f06000c;
-        public static final int where=0x7f060005;
+        public static final int backpic=0x7f06000f;
+        public static final int cancel=0x7f06000e;
+        public static final int desc=0x7f060002;
+        public static final int editeventname=0x7f060005;
+        public static final int end_date=0x7f06000b;
+        public static final int eventname=0x7f060004;
+        public static final int hello_world=0x7f060003;
+        public static final int show_date=0x7f06000c;
+        public static final int show_end_date=0x7f060009;
+        public static final int show_start_date=0x7f060008;
+        public static final int show_where=0x7f060007;
+        public static final int start_date=0x7f06000a;
+        public static final int submit=0x7f06000d;
+        public static final int where=0x7f060006;
     }
     public static final class style {
         /** 
