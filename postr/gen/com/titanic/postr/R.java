717ab4c1ebb5b244658171d1454ce8c3002a1101
Ashna Ashok <aashok@andew.cmu.edu>
2014-02-01 01:40:56 -0500

    changing button UI

--- postr/gen/com/titanic/postr/R.java
+++ postr/gen/com/titanic/postr/R.java
@@ -40,7 +40,8 @@ public final class R {
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int form_layout=0x7f030001;
+        public static final int activity_splash=0x7f030001;
+        public static final int form_layout=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f070000;
