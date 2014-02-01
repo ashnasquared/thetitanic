6faa4626f214527fb74e9e84902dc920323a0624
Naomi Rubin <nhrubin@andrew.cmu.edu>
2014-01-31 20:36:29 -0500

    Added a line to call the form layout screen in the camera callback

---
+++ postr/src/com/titanic/postr/FormActivity.java
@@ -0,0 +1,11 @@
+package com.titanic.postr;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FormActivity extends Activity {
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.form_layout);
+	}
+}
--- postr/src/com/titanic/postr/MainActivity.java
+++ postr/src/com/titanic/postr/MainActivity.java
@@ -3,6 +3,7 @@ package com.titanic.postr;
 import java.io.File;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -33,11 +34,16 @@ public class MainActivity extends Activity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    	final Context context = this;
         if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
             if (resultCode == RESULT_OK) {
                 // Image captured and saved to fileUri specified in the Intent
                 Toast.makeText(this, "Image saved to:\n" +
                          data.getData(), Toast.LENGTH_LONG).show();
+                
+                
+                Intent intent = new Intent(context, FormActivity.class);
+                startActivity(intent); 
             } else if (resultCode == RESULT_CANCELED) {
                 // User cancelled the image capture
             } else {
